615abbc0b74c330fdc77a26564894b26bb2e8ccf
Aman Ansari <amanansari1134@gmail.com>
2020-01-24 22:15:35 +0545

    Initial commit

---
+++ Alphabets samall and large.py
@@ -0,0 +1,8 @@
+capitala=ord('A')
+capitalz=ord('Z')
+smalla=ord('a')
+smallz=ord('z')
+for i in range(capitala,capitalz+1):
+    print(chr(capitala),chr(smalla),end='    ')
+    capitala=capitala+1
+    smalla+=1
